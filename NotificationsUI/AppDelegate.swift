6efa203890b79326926a30a011b70805da3f86bb
ayushm-webonise <ayush.mahajan@weboniselab.com>
2017-09-07 11:37:32 +0530

    adds project files to the repo

---
+++ NotificationsUI/AppDelegate.swift
@@ -0,0 +1,62 @@
+//
+//  AppDelegate.swift
+//  NotificationsUI
+//
+//  Created by Pranjal Satija on 9/12/16.
+//  Copyright © 2016 Pranjal Satija. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+
+@UIApplicationMain class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound]) {(accepted, error) in
+            if !accepted {
+                print("Notification access denied.")
+            }
+        }
+        
+        return true
+    }
+    
+    func scheduleNotification(at date: Date) {
+        
+        let calendar = Calendar(identifier: .gregorian)
+        let components = calendar.dateComponents(in: .current, from: date)
+        let newComponents = DateComponents(calendar: calendar, timeZone: .current, month: components.month,
+                                           day: components.day, hour: components.hour, minute: components.minute)
+        let trigger = UNCalendarNotificationTrigger(dateMatching: newComponents, repeats: false)
+        
+        //create the notification content
+        let content = UNMutableNotificationContent()
+        content.title = "Lunch Reminder"
+        content.body = "Just a reminder to eat your lunch at 3:00 pm"
+        content.sound = UNNotificationSound.default()
+        
+        //add an attachment to the notification
+        if let path = Bundle.main.path(forResource: "rotlu", ofType: "gif") {
+            let url = URL(fileURLWithPath: path)
+            
+            do {
+                let attachment = try UNNotificationAttachment(identifier: "logo", url: url, options: nil)
+                content.attachments = [attachment]
+            } catch {
+                print("The attachment was not loaded.")
+            }
+        }
+        
+        //create a notification request
+        let request = UNNotificationRequest(identifier: "textNotification", content: content, trigger: trigger)
+        
+        UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
+        UNUserNotificationCenter.current().add(request) {(error) in
+            if let error = error {
+                print("Uh oh! We had an error: \(error)")
+            }
+        }
+    }
+}
---
+++ NotificationsUI/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  NotificationsUI
+//
+//  Created by Pranjal Satija on 9/12/16.
+//  Copyright © 2016 Pranjal Satija. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBAction func datePickerDidSelectNewDate(_ sender: UIDatePicker) {
+        
+        let selectedDate = sender.date
+        let delegate = UIApplication.shared.delegate as? AppDelegate
+        delegate?.scheduleNotification(at: selectedDate)
+    }
+}
